ecce8425f6307723a99e0f291b9403727f13fec7
Yifan <yifan.zhu.1111@hotmail.com>
2021-10-30 03:41:22 +0800

    Add route

--- MyDiary/app/src/main/java/com/example/mydiary/ui/NavigationActivity.java
+++ MyDiary/app/src/main/java/com/example/mydiary/ui/NavigationActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +16,8 @@ import com.mapbox.android.core.location.LocationEnginePriority;
 import com.mapbox.android.core.location.LocationEngineProvider;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
@@ -27,8 +30,16 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class NavigationActivity extends AppCompatActivity implements OnMapReadyCallback, MapboxMap.OnMapClickListener, LocationEngineListener, PermissionsListener {
 
@@ -42,6 +53,8 @@ public class NavigationActivity extends AppCompatActivity implements OnMapReadyC
     private Point originPosition;
     private Point destinationPosition;
     private Marker destinationMarker;
+    private NavigationMapRoute navigationMapRoute;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +68,8 @@ public class NavigationActivity extends AppCompatActivity implements OnMapReadyC
         startButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                NavigationLauncherOptions options = NavigationLauncherOptions.builder().origin(originPosition).destination(destinationPosition).shouldSimulateRoute(true).build();
+                NavigationLauncher.startNavigation(NavigationActivity.this, options);
             }
         });
     }
@@ -112,8 +126,37 @@ public class NavigationActivity extends AppCompatActivity implements OnMapReadyC
         destinationMarker = map.addMarker(new MarkerOptions().position(point));
         destinationPosition = Point.fromLngLat(point.getLongitude(), point.getLatitude());
         originPosition = Point.fromLngLat(originLocation.getLongitude(), originLocation.getLatitude());
+        getRoute(originPosition, destinationPosition);
         startButton.setEnabled(true);
         startButton.setBackgroundResource(R.color.mapboxBlue);
+    }
+
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder().accessToken(Mapbox.getAccessToken()).origin(origin).destination(destination).build().getRoute(new Callback<DirectionsResponse>() {
+            @Override
+            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                if (response.body() == null) {
+                    Log.e(TAG, "No route");
+                    return;
+                } else if (response.body().routes().size() == 0) {
+                    Log.e(TAG, "No route");
+                    return;
+                }
+                DirectionsRoute currentRoute = response.body().routes().get(0);
+
+                if (navigationMapRoute != null) {
+                    navigationMapRoute.removeRoute();
+                } else {
+                    navigationMapRoute = new NavigationMapRoute(null, mapView, map);
+                }
+                navigationMapRoute.addRoute(currentRoute);
+            }
+
+            @Override
+            public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                Log.e(TAG, t.getMessage());
+            }
+        });
     }
 
     @Override
