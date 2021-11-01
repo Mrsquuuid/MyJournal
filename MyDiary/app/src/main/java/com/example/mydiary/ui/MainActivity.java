8fdd224d89a96d556c0c00e5c7f69fe9b251c624
yuqiuwang <yuqiu1@student.unimelb.edu.au>
2021-11-01 18:25:56 +0800

    show current date

--- MyDiary/app/src/main/java/com/example/mydiary/ui/MainActivity.java
+++ MyDiary/app/src/main/java/com/example/mydiary/ui/MainActivity.java
@@ -23,6 +23,8 @@ import com.example.mydiary.db.ImageDatabaseHelper;
 
 import com.example.mydiary.R;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -49,11 +51,19 @@ public class MainActivity extends AppCompatActivity {
         //calendar
         calendarView=findViewById(R.id.calendar);
         textView=findViewById(R.id.textview);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        int curruntMonth = month +1;
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        String date = day+"/"+curruntMonth+"/"+year;
+        textView.setText(date);
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String date = dayOfMonth+"/"+month+"/"+year;
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int day) {
+                month = month+1;
+                String date = day+"/"+month+"/"+year;
                 textView.setText(date);
             }
         });
