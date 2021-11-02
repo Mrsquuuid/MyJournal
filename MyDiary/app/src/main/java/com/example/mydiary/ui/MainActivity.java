700da0556bbe978f99a87cf7c23c19f55083f217
Hailin-xiong <hailinx@student.unimelb.edu.au>
2021-11-02 21:11:52 +1100

    An update on MainActivity.

--- MyDiary/app/src/main/java/com/example/mydiary/ui/MainActivity.java
+++ MyDiary/app/src/main/java/com/example/mydiary/ui/MainActivity.java
@@ -29,6 +29,7 @@ import com.example.mydiary.db.ImageDatabaseHelper;
 import com.example.mydiary.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
@@ -59,11 +60,18 @@ public class MainActivity extends AppCompatActivity {
         //calendar
         calendarView=findViewById(R.id.calendar);
         textView=findViewById(R.id.textview);
-
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        int curruntMonth = month +1;
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        String date = day+"/"+curruntMonth+"/"+year;
+        textView.setText(date);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String date = dayOfMonth+"/"+month+"/"+year;
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int day) {
+                month = month+1;
+                String date = day+"/"+month+"/"+year;
                 textView.setText(date);
             }
         });
